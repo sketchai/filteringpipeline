dec3c5e7e8a0e5200bd0c1d96712180b4df3985c
Frederique ROBIN <frederique.robin@edf.fr>
2022-04-06 17:03:24 +0200

    mirror

--- tests/unit/test_checklabel.py
+++ tests/unit/test_checklabel.py
@@ -1,11 +1,14 @@
-import unittest
-import logging
-
-
-from src.sketchgraphs.sketchgraphs.data.sequence import NodeOp, EdgeOp
-from src.sketchgraphs.sketchgraphs.data import sketch as datalib
+import sys
+sys.path.append('src/sketchgraphs/')
+sys.path.append('src/filtering-pipeline/')
 
 from src.filters.filter_checklabel import FilterCheckLabel
+from filtering_pipeline import KO_FILTER_TAG
+from sketchgraphs.data import sketch as datalib
+from sketchgraphs.data.sequence import NodeOp, EdgeOp
+import logging
+import unittest
+
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
@@ -14,27 +17,27 @@ logger = logging.getLogger()
 class TestFilterCheckLabel(unittest.TestCase):
 
     def test_process(self):
-        filter = FilterCheckLabel(conf_filter={'label_list': [datalib.ConstraintType.Coincident, datalib.ConstraintType.Distance, datalib.ConstraintType.Horizontal,
-                                                              datalib.EntityType.Point, datalib.EntityType.Line, datalib.EntityType.Circle]})
+        filter1 = FilterCheckLabel(conf_filter={
+            'node_label_list': [datalib.EntityType.Point, datalib.EntityType.Line, datalib.EntityType.Circle],
+            'edge_label_list': [datalib.ConstraintType.Coincident, datalib.ConstraintType.Distance, datalib.ConstraintType.Horizontal]
+        })
 
         # Test 1 : the op fulfills the condition
         message_A = {'op': NodeOp(label=0)}
-        message = filter.process(message_A)
-        message_A['status'] = True
-        self.assertDictEqual(message_A, message)
+        answer = filter1.process(message_A)
+        self.assertIsNone(answer.get(KO_FILTER_TAG))
 
         message_A = {'op': EdgeOp(label=0, references=(1,))}
-        message = filter.process(message_A)
-        message_A['status'] = True
-        self.assertDictEqual(message_A, message)
+        answer = filter1.process(message_A)
+        self.assertIsNone(answer.get(KO_FILTER_TAG))
 
         # Test 2 : the op does not fulfill the condition
         message_A = {'op': NodeOp(label=12)}
-        message = filter.process(message_A)
-        message_A['status'] = False
-        self.assertDictEqual(message_A, message)
+        answer = filter1.process(message_A)
+        tag = answer.get(KO_FILTER_TAG)
+        self.assertEqual(tag, filter1.name)
 
         message_A = {'op': EdgeOp(label=12, references=(1,))}
-        message = filter.process(message_A)
-        message_A['status'] = False
-        self.assertDictEqual(message_A, message)
+        answer = filter1.process(message_A)
+        tag = answer.get(KO_FILTER_TAG)
+        self.assertEqual(tag, filter1.name)
